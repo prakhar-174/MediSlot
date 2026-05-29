246cfc6338ebc1b092585ab2628b7fa451a4e40b
prakhar-174 <prakhartiwari174@gmail.com>
2026-05-29 20:11:02 +0530

    bug fix

--- FrontendMediSlot/src/utils/api.js
+++ FrontendMediSlot/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL || '/api';
+const API_BASE = '/api';
 
 export const fetchApi = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
